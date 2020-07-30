13d3c66f755024f45367d03b91e204b26d964adf
AmayaSantos <samaya@fi.uba.ar>
2020-07-30 12:17:45 -0300

    inicio de Carpeta Ronda

---
+++ src/main/java/edu/fiuba/algo3/modelo/Ronda/Ronda.java
@@ -0,0 +1,31 @@
+package edu.fiuba.algo3.modelo.Ronda;
+
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.Pregunta;
+import edu.fiuba.algo3.modelo.puntaje.Puntaje;
+
+public class Ronda {
+    public calificarEn(Jugador jugador, Pregunta pregunta){
+        Puntaje puntaje=pregunta.calificar(jugador.respuestas());
+
+        jugador.sumar(puntaje);
+    }
+    /*
+    public calificarEn(Jugadores jugadores,Pregunta pregunta){
+        Puntaje puntaje=pregunta.calificar(jugador.respuestas());
+        jugador.sumarTemporalemente(puntaje);
+
+
+
+    }
+    private boolean comprobarImplementacionExclusividad() {
+        Puntajes puntajesTemporales= new Puntajes();
+        for (Jugador jugador: jugadores) {
+             puntajesTemporales.add(jugador.puntajeTemporal());
+        }
+        return puntajesTemporales.contains(new PuntoNulo);
+    }
+
+
+     */
+}
