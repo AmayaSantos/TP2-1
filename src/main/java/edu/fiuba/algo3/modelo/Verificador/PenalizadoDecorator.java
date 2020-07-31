ec03a4a9df774a616cb9ddf978bb9fd293d9fa3a
Matuzaramay <matiasarielfigueroa@gmail.com>
2020-07-30 21:20:28 -0300

    Se agrega Penalizado Decorator, mal implementado

---
+++ src/main/java/edu/fiuba/algo3/modelo/Verificador/PenalizadoDecorator.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.modelo.Verificador;
+
+import edu.fiuba.algo3.modelo.Opciones.Opciones;
+import edu.fiuba.algo3.modelo.puntaje.Puntaje;
+import edu.fiuba.algo3.modelo.puntaje.PuntoNulo;
+
+public class PenalizadoDecorator extends VerificadorDecorator{
+
+    public static PenalizadoDecorator ClasicoDecorator(Verificador verificador){
+        return new PenalizadoDecorator();
+    }
+
+    public Puntaje calificar(Opciones opcionesCorrectas, Opciones opcionesIncorrectas){
+        return new PuntoNulo();
+    }
+
+}
