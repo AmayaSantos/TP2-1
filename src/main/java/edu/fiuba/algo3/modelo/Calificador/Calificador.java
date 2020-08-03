69e200bf142d31eff0ba1917733614601e764c94
Matuzaramay <matiasarielfigueroa@gmail.com>
2020-08-02 22:04:23 -0300

    Se agrega clase abstracta Calificador

---
+++ src/main/java/edu/fiuba/algo3/modelo/Calificador/Calificador.java
@@ -0,0 +1,14 @@
+package edu.fiuba.algo3.modelo.Calificador;
+
+import edu.fiuba.algo3.modelo.Opciones.Opciones;
+import edu.fiuba.algo3.modelo.puntaje.Puntaje;
+
+public abstract class Calificador {
+    Calificador siguienteCalificador;
+
+    public void setearSiguiente(Calificador siguienteCalificador){
+        this.siguienteCalificador = siguienteCalificador;
+    }
+
+    public abstract Puntaje calificar(Opciones correctas, Opciones elegidas);
+}
